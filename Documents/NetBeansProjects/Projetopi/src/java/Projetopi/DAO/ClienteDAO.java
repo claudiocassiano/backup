b436c6f7fe15e91ad349f97f89966ff122bf8583
Claudio Cassiano <claudiopecassiano@gmail.com>
2017-06-05 21:49:13 -0300

    Até aqui funciona. Tudo OK.

--- Documents/NetBeansProjects/Projetopi/src/java/Projetopi/DAO/ClienteDAO.java
+++ Documents/NetBeansProjects/Projetopi/src/java/Projetopi/DAO/ClienteDAO.java
@@ -72,9 +72,5 @@ public class ClienteDAO implements interfaceCliente {
 
     }
 
-    //@Override
-    //public void adicionar(Cliente cliente) {
-     //   throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-   // }
 
 }
